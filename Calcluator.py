c876e4df0a45f54754ef495583587a1a2e30ce79
arusnmi <parush.larty@gmail.com>
2025-04-19 20:58:11 +0530

    basic calc done

---
+++ Calcluator.py
@@ -0,0 +1,6 @@
+Calc_choice=input("Enter the calcluator you want (basic, finction, derivitive and intragtion): ")
+
+if Calc_choice == "basic":
+    print(eval(input("Enter the calcluation: ")))
+
+
